ec1d9339856c606c91d6c09f6eaf2079eba3a549
Omar Siddiqui <buctik@hotmail.com>
2011-11-30 22:09:45 -0800

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.1.1'
   gem 'uglifier', '>= 1.0.3'
 end
+
 gem 'jquery-rails'
 gem "rspec-rails", ">= 2.7.0", :group => [:development, :test]
 gem 'annotate', '2.4.0', :group => [:development]
@@ -19,3 +20,5 @@ gem "capybara", ">= 1.1.1", :group => :test
 gem "database_cleaner", ">= 0.6.7", :group => :test
 gem "launchy", ">= 2.0.5", :group => :test
 gem "devise", ">= 1.4.9"
+gem 'spork', '0.9.0.rc8', :group => :test
+gem 'gravatar_image_tag', '1.0.0.pre2'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,39 @@
 require 'spec_helper'
 
 describe UsersController do
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
+  render_views
+  
+  describe "GET 'show'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    it "should be successful" do
+      get :show, :id => @user
       response.should be_success
     end
+    
+    it "should have the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+    
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title, :content => @user.name")
+    end
+    
+    it "should have the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+    
   end
 
 end
